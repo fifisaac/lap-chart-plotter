7f03066d0654a4f691a40d7977a2ce47e855a110
fifisaac <99099211+fifisaac@users.noreply.github.com>
2024-04-01 20:57:58 +0100

    added tkinter gui

--- plot.py
+++ plot.py
@@ -1,43 +1,96 @@
 ## TO-DO
 ## Find an effective way to show cars with multiple drivers
 ## Handle sessions other than race and weekends with many races
-## GUI
+## Tidy up horrible GUI (both aesthetically and in code)
 ## Animate the graph
 
 import matplotlib.pyplot as plt
 import requests
-import tkinter
+import tkinter as tk
+from tkinter import ttk, messagebox
 
-def get_slug():
-    decade = input('decade: ')
-    r = requests.get(f'https://motorsportstats.com/api/advanced-search?entity=series&size=999&filterIds={decade}s')
-    data = r.json()
+def tkinter():
+    root = tk.Tk()
+    root.title('Lap Chart Plotter')
 
-    series_names = [(i['name'], i['uuid']) for i in data['content']]
-    print([i[0] for i in series_names])
+    root.geometry('300x240')
 
-    series_name = input('name: ')
-    for i in series_names:
-        if i[0] == series_name:
-            series_uuid = i[1]
-            break
-    year = input('year: ')
-    print(f'https://motorsportstats.com/api/advanced-search?entity=events&size=999&filterIds={series_uuid}&filterIds={year}')
-    r = requests.get(f'https://motorsportstats.com/api/advanced-search?entity=events&size=999&filterIds={series_uuid}&filterIds={year}')
-    data = r.json()
+    def reset():
+        root.destroy()
+        tkinter()
 
-    race_names = [(i['name'], i['uuid']) for i in data['content']]
-    print([i[0] for i in race_names])
-    race_name = input('name: ')
-    for i in race_names:
-        if i[0] == race_name:
-            race_uuid = i[1]
-            break
 
-    race = f'https://motorsportstats.com/api/result-statistics?sessionSlug={race_uuid}_race&sessionFact=LapChart&size=999'
-    
-    return (plot(race))
 
+    resetButton = tk.Button(root, text='Reset', command=reset).place(x=125, y=200)
+    #resetButton.pack()
+
+    def series(seriesUUID, decadeVar, boxSeries):
+
+        def race(yearVar):
+            boxYear.configure(state='disabled')
+
+            def load(raceVar):
+                boxRace.configure(state='disabled')
+                try:
+                    plot(f'https://motorsportstats.com/api/result-statistics?sessionSlug={dictRace[raceVar]}_race&sessionFact=LapChart&size=999')
+                    root.destroy()
+                    tkinter()
+                except:
+                    messagebox.showerror("Error", "An error occured. The lap chart likely doesn't exist for this race")
+                    root.destroy()
+                    tkinter()
+
+
+            r = requests.get(f'https://motorsportstats.com/api/advanced-search?entity=events&size=999&filterIds={seriesUUID}&filterIds={yearVar}')
+            data = r.json()
+            if data['totalElements'] == 0:
+                messagebox.showerror("Error", "An error occured. The lap chart likely doesn't exist for this race")
+                root.destroy()
+                tkinter()
+
+            dictRace = dict((i['name'], i['uuid']) for i in data['content'])
+
+            raceVar = tk.StringVar()
+            raceVar.set("")
+
+            boxRace = ttk.OptionMenu(root, raceVar, "Select a race", *dictRace.keys(), command=load)
+            boxRace.pack(pady=10)
+
+
+
+
+        boxSeries.configure(state='disabled')
+        yearVar = tk.StringVar(root)
+        yearVar.set("")
+
+        years = [int(str(decadeVar[:3]) + str(i)) for i in range(0,10)]
+        boxYear = ttk.OptionMenu(root, yearVar, "Select a year", *years, command=race)
+        boxYear.pack(pady=10)
+
+    def decade(decadeVar):
+        boxDecade.configure(state='disabled')
+        def submit(seriesVar):
+            series(dictSeries[seriesVar], decadeVar, boxSeries)
+
+        r = requests.get(f'https://motorsportstats.com/api/advanced-search?entity=series&size=999&filterIds={decadeVar}')
+        data = r.json()
+        dictSeries = dict((i['name'], i['uuid']) for i in data['content'])
+
+
+        seriesVar = tk.StringVar(root)
+        seriesVar.set("")
+
+        boxSeries = ttk.OptionMenu(root, seriesVar, "Select a series", *dictSeries.keys(), command=submit)
+        boxSeries.pack(pady=10)
+
+    decadeVar = tk.StringVar(root)
+    decadeVar.set("")
+
+    boxDecade = ttk.OptionMenu(root, decadeVar, "Select a decade", "2020s", "2010s", "2000s", "1990s", "1980s", "1970s", "1960s", "1950s", command=decade)
+    boxDecade.pack(pady=10)
+
+
+    root.mainloop()
 
 def plot(url):
 
@@ -70,4 +123,5 @@ def plot(url):
     plt.show()
 
 if __name__ == '__main__':
-    get_slug()
+    tkinter()
+    #get_slug()
