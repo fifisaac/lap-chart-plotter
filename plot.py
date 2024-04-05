18a228fb0c42000f164e315f929a85955704181e
fifisaac <99099211+fifisaac@users.noreply.github.com>
2024-04-05 11:08:25 +0100

    moved gui to a class (and other minor chages)

--- plot.py
+++ plot.py
@@ -1,6 +1,7 @@
 ## TO-DO
 ## Find an effective way to show cars with multiple drivers
-## Tidy up horrible GUI (both aesthetically and in code)
+## Tidy up GUI
+## Include other types of graphs. See Issue #1
 ## Animate the graph
 
 import matplotlib.pyplot as plt
@@ -8,107 +9,110 @@ import requests
 import tkinter as tk
 from tkinter import ttk, messagebox
 
-def gui(): #bodged up gui code
-    root = tk.Tk()
-    root.title('Lap Chart Plotter')
+class GUI:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title('Lap Chart Plotter')
 
-    root.geometry('300x300')
+        self.root.geometry('300x300')
+        self.root.resizable(False, False)
 
-    def reset():
-        root.destroy()
-        gui()
+        resetButton = tk.Button(self.root, text='Reset', command=self.reset)
+        resetButton.place(x=175, y=240)
+        self.lapChartButton = tk.Button(self.root, text='Load Lap Chart', command=self.loadLapChart)
+        self.lapChartButton.place(x=75, y=240)
+        self.lapChartButton.configure(state='disabled')
 
-    resetButton = tk.Button(root, text='Reset', command=reset).place(x=125, y=240)
+        self.chooseDecade()
 
-    def series(seriesUUID, decadeVar, boxSeries):
+        tk.mainloop()
+    
+    def reset(self):
+            self.root.destroy()
+            GUI()
+    
+    def chooseDecade(self):
+        self.decadeVar = tk.StringVar(self.root)
+        self.decadeVar.set("")
 
-        def race(yearVar):
-            boxYear.configure(state='disabled')
+        self.boxDecade = ttk.OptionMenu(self.root, self.decadeVar, "Select a decade", "2020s", "2010s", "2000s", "1990s", "1980s", "1970s", "1960s", "1950s", command=self.chooseSeries)
+        self.boxDecade.pack(pady=10)
+    
+    def chooseSeries(self, decade: str):
+        self.boxDecade.configure(state='disabled')
 
-            def session(raceVar):
-                boxRace.configure(state='disabled')
-
-                sessionNames = ['race', 'race-1', 'race-2', 'race-3', 'race-4', 'race-5'] # the only way to find available sessions seems to be trial and error using possible session names. only races are available as quali/practice tends to have strange data that doesn't work on the graph
-                validSessions = []
-
-                def load(sessionVar):
-                    boxSession.configure(state='disabled')
-
-                    try:
-                        plot(f'https://motorsportstats.com/api/result-statistics?sessionSlug={dictRace[raceVar]}_{sessionVar}&sessionFact=LapChart&size=999', raceVar, yearVar, sessionVar)
-                        root.destroy()
-                        gui()
-                    except:
-                        messagebox.showerror("Error", "An error occured. The lap chart likely doesn't exist for this race")
-                        reset()
-
-                for i in sessionNames:
-                    try:
-                        r = requests.get(f'https://motorsportstats.com/api/result-statistics?sessionSlug={dictRace[raceVar]}_{i}&sessionFact=LapChart&size=999')
-                        data = r.json()
-                        validSessions.append(i)
-                    except:
-                         pass
-
-                if validSessions == []:
-                    messagebox.showerror("Error", "An error occured. The lap chart likely doesn't exist for this race")
-                    reset()
-
-                
-                sessionVar = tk.StringVar()
-                sessionVar.set("")
-
-                boxSession = ttk.OptionMenu(root, sessionVar, "Select a session", *validSessions, command=load)
-                boxSession.pack(pady=10)
-
-
-            r = requests.get(f'https://motorsportstats.com/api/advanced-search?entity=events&size=999&filterIds={seriesUUID}&filterIds={yearVar}')
-            data = r.json()
-            if data['totalElements'] == 0:
-                messagebox.showerror("Error", "An error occured. The lap chart likely doesn't exist for this race")
-                root.destroy()
-                gui()
-
-            dictRace = dict((i['name'], i['uuid']) for i in data['content'])
-
-            raceVar = tk.StringVar()
-            raceVar.set("")
-
-            boxRace = ttk.OptionMenu(root, raceVar, "Select a race", *dictRace.keys(), command=session)
-            boxRace.pack(pady=10)
-
-        boxSeries.configure(state='disabled')
-        yearVar = tk.StringVar(root)
-        yearVar.set("")
-
-        years = [int(str(decadeVar[:3]) + str(i)) for i in range(0,10)]
-        boxYear = ttk.OptionMenu(root, yearVar, "Select a year", *years, command=race)
-        boxYear.pack(pady=10)
-
-    def decade(decadeVar):
-        boxDecade.configure(state='disabled')
-        def submit(seriesVar):
-            series(dictSeries[seriesVar], decadeVar, boxSeries)
-
-        r = requests.get(f'https://motorsportstats.com/api/advanced-search?entity=series&size=999&filterIds={decadeVar}')
+        r = requests.get(f'https://motorsportstats.com/api/advanced-search?entity=series&size=999&filterIds={decade}')
         data = r.json()
-        dictSeries = dict((i['name'], i['uuid']) for i in data['content'])
+        self.dictSeries = dict((i['name'], i['uuid']) for i in data['content'])
 
 
-        seriesVar = tk.StringVar(root)
-        seriesVar.set("")
+        self.seriesVar = tk.StringVar(self.root)
+        self.seriesVar.set("")
 
-        boxSeries = ttk.OptionMenu(root, seriesVar, "Select a series", *dictSeries.keys(), command=submit)
-        boxSeries.pack(pady=10)
+        self.boxSeries = ttk.OptionMenu(self.root, self.seriesVar, "Select a series", *self.dictSeries.keys(), command=self.chooseYear)
+        self.boxSeries.pack(pady=10)
 
-    decadeVar = tk.StringVar(root)
-    decadeVar.set("")
+    def chooseYear(self, series: str):
+        self.boxSeries.configure(state='disabled')
+        self.yearVar = tk.StringVar(self.root)
+        self.yearVar.set("")
 
-    boxDecade = ttk.OptionMenu(root, decadeVar, "Select a decade", "2020s", "2010s", "2000s", "1990s", "1980s", "1970s", "1960s", "1950s", command=decade)
-    boxDecade.pack(pady=10)
+        years = [int(str(self.decadeVar.get()[:3]) + str(i)) for i in range(0,10)]
+        self.boxYear = ttk.OptionMenu(self.root, self.yearVar, "Select a year", *years, command=self.chooseRace)
+        self.boxYear.pack(pady=10)
+    
+    def chooseRace(self, year):
+        self.boxYear.configure(state='disabled')
 
+        seriesUUID = self.dictSeries[self.seriesVar.get()]
 
-    root.mainloop()
+        r = requests.get(f'https://motorsportstats.com/api/advanced-search?entity=events&size=999&filterIds={seriesUUID}&filterIds={year}')
+        data = r.json()
+        if data['totalElements'] == 0:
+            messagebox.showerror("Error", "An error occured. The lap chart likely doesn't exist for this race")
+            self.reset()
+
+        self.dictRace = dict((i['name'], i['uuid']) for i in data['content'])
+
+        self.raceVar = tk.StringVar()
+        self.raceVar.set("")
+
+        self.boxRace = ttk.OptionMenu(self.root, self.raceVar, "Select a race", *self.dictRace.keys(), command=self.chooseSession)
+        self.boxRace.pack(pady=10)
+        
+    def chooseSession(self, race):
+        self.boxRace.configure(state='disabled')
+
+        sessionNames = ['race', 'race-1', 'race-2', 'race-3', 'race-4', 'race-5'] # the only way to find available sessions seems to be trial and error using possible session names. only races are available as quali/practice tends to have strange data that doesn't work on the graph
+        validSessions = []
+        for i in sessionNames:
+            try:
+                r = requests.get(f'https://motorsportstats.com/api/result-statistics?sessionSlug={self.dictRace[self.raceVar.get()]}_{i}&sessionFact=LapChart&size=999')
+                data = r.json()
+                validSessions.append(i)
+            except:
+                    pass
+
+        if validSessions == []:
+            messagebox.showerror("Error", "An error occured. The lap chart likely doesn't exist for this race")
+            self.reset()
+
+        
+        self.sessionVar = tk.StringVar()
+        self.sessionVar.set("")
+
+        boxSession = ttk.OptionMenu(self.root, self.sessionVar, "Select a session", *validSessions, command=self.enableButton)
+        boxSession.pack(pady=10)
+    
+    def enableButton(self, session): ##session value is ignored
+        self.lapChartButton.config(state='normal')
+    
+    def loadLapChart(self):
+        try:
+            plot(f'https://motorsportstats.com/api/result-statistics?sessionSlug={self.dictRace[self.raceVar.get()]}_{self.sessionVar.get()}&sessionFact=LapChart&size=999', self.raceVar.get(), self.yearVar.get(), self.sessionVar.get())
+        except:
+            messagebox.showerror("Error", "An error occured. The lap chart likely doesn't exist for this race")
+
 
 def plot(url, name, year, session):
 
@@ -120,11 +124,8 @@ def plot(url, name, year, session):
                     pos.append(i+1)
         
         return pos
-    try:
-        r = requests.get(url)
-    except:
-        print('Unable to access lap chart')
-        return
+    
+    r = requests.get(url)
     data = r.json()
 
     cars = [[i['carNumber'], i['drivers'][0]['name']] for i in data['cars']] ##only one name will show for cars with multiple drivers
@@ -134,7 +135,7 @@ def plot(url, name, year, session):
     for i, j in enumerate(positions):
         ax.plot(positions[i], label = f'#{cars[i][0]} - {cars[i][1]}')
         ax.annotate(f'#{cars[i][0]}', xy=(len(positions[i])-0.8,positions[i][-1]+0.1))
-    ax.set_xticks([0, data['content'][-1]['lap']])
+    ax.set_xticks(range(0, data['content'][-1]['lap']+1, 10))
     ax.set_yticks(range(1, len(cars) +1))
     ax.invert_yaxis()
     ax.set_xlim(left = 0)
@@ -145,4 +146,5 @@ def plot(url, name, year, session):
     plt.show()
 
 if __name__ == '__main__':
-    gui()
+    #gui()
+    GUI()
